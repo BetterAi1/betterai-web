54fc222eb353614fc2128562d1808f43f51187dd
Addri21 <adrivet2001@gmail.com>
2026-06-25 22:41:57 +0200

    Scroll to top on route change
    Aviso Legal, Política de Privacidad and Cookies otherwise kept
    whatever scroll position the user had on the previous page, so
    navigating there from deep in the homepage skipped the heading.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import AvisoLegal from "./pages/AvisoLegal";
 import PoliticaPrivacidad from "./pages/PoliticaPrivacidad";
@@ -7,6 +8,7 @@ import Cookies from "./pages/Cookies";
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/aviso-legal" element={<AvisoLegal/>}/>
